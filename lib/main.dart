b022e946bf9e124eabdbf4eb285dc6ca62a382aa
Heliomar Marques <heliomarpm@gmail.com>
2021-11-16 23:12:33 -0300

    adicionado formulario de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: const ListaTransferencias(),
-        appBar: AppBar(
-          title: const Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => print('Adicionar'),
-          child: const Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final _controladorCampoNumeroConta = TextEditingController();
+  final _controladorCampoValor = TextEditingController();
+
+  FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                style: const TextStyle(fontSize: 24.0),
+                decoration: const InputDecoration(
+                    labelText: 'Número da conta', hintText: '0000'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: const TextStyle(fontSize: 24.0),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: const Text('Confirmar'),
+              onPressed: () {
+                debugPrint('clicou no confirmar');
+                final numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+                final valor = double.tryParse(_controladorCampoValor.text);
+                if (numeroConta != null && valor != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('$transferenciaCriada');
+                }
+              },
+            )
+          ],
+        ));
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   const ListaTransferencias({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 1000)),
-        ItemTransferencia(Transferencia(200.0, 2000)),
-        ItemTransferencia(Transferencia(300.0, 3000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   const ItemTransferencia(this._transferencia, {Key? key}) : super(key: key);
@@ -50,4 +113,9 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
